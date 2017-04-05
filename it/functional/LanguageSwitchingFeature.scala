c3c18194f74ecb1bb40a2593ca31f3c60927dd38
jose.puente <jose.puente@digital.hmrc.gov.uk>
2017-04-05 14:41:45 +0100

    Fix Language Switching feature

--- it/functional/LanguageSwitchingFeature.scala
+++ it/functional/LanguageSwitchingFeature.scala
@@ -8,7 +8,7 @@ import org.scalatest.junit.JUnitRunner
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.{Application, Mode}
 import support.StubbedFeatureSpec
-import support.page.CookiesPage
+import support.page.{EnglishCookiesPage, WelshCookiesPage}
 
 @RunWith(classOf[JUnitRunner])
 class LanguageSwitchingFeature extends StubbedFeatureSpec {
@@ -27,23 +27,25 @@ class LanguageSwitchingFeature extends StubbedFeatureSpec {
     scenario("Switch from English to Welsh in the cookies page") {
       WireMock.stubFor(post(urlEqualTo("/write/audit")).willReturn(aResponse().withStatus(200)))
 
-      Given("I go to the cookies page")
-      goOn(CookiesPage)
+      When("I go to the English version of the cookies page")
+      goOn(EnglishCookiesPage)
 
-      And("The language switching link is visible")
-      CookiesPage.languageSwitchingLink.isDefined shouldBe true
+      Then("The language switching link is visible")
+      EnglishCookiesPage.hasLanguageSwitchingLink shouldBe true
 
-      And("I click on the switch language link")
-      click on linkText("Cymraeg")
+      When("I click on the switch language link")
+      EnglishCookiesPage.switchLanguage()
 
       Then("I see the page in Welsh")
-      CookiesPage.cookiesInfoText shouldBe "Caiff ffeiliau bach (a elwir yn 'cwcis') eu gosod ar eich cyfrifiadur i gasglu gwybodaeth am sut yr ydych yn pori'r wefan."
+      on(WelshCookiesPage)
+      WelshCookiesPage.cookiesInfoText shouldBe "Caiff ffeiliau bach (a elwir yn 'cwcis') eu gosod ar eich cyfrifiadur i gasglu gwybodaeth am sut yr ydych yn pori'r wefan."
 
-      And("I click on the switch language link")
-      click on linkText("English")
+      When("I click on the switch language link")
+      WelshCookiesPage.switchLanguage()
 
       Then("I see the page in English")
-      CookiesPage.cookiesInfoText shouldBe "Small files (known as 'cookies') are put onto your computer to collect information about how you browse the site."
+      on(EnglishCookiesPage)
+      EnglishCookiesPage.cookiesInfoText shouldBe "Small files (known as 'cookies') are put onto your computer to collect information about how you browse the site."
     }
   }
 }
@@ -74,10 +76,10 @@ class LanguageSwitchingDisabledFeature extends StubbedFeatureSpec {
       WireMock.stubFor(post(urlEqualTo("/write/audit")).willReturn(aResponse().withStatus(200)))
 
       Given("I go to the cookies page")
-      goOn(CookiesPage)
+      goOn(EnglishCookiesPage)
 
       Then("I do not see the language switching link")
-      CookiesPage.languageSwitchingLink shouldBe None
+      EnglishCookiesPage.hasLanguageSwitchingLink shouldBe false
     }
   }
 }
