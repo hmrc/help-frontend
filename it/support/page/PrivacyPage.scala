c6c50718b27c44949823d8779b39fc6b4bc4d34d
Adam Fenwick <fenwicka@OB1295A.local>
2018-05-24 13:39:06 +0100

    Updating privacy policy tests

--- it/support/page/PrivacyPage.scala
+++ it/support/page/PrivacyPage.scala
@@ -4,8 +4,10 @@ import org.openqa.selenium.By
 import support.Env
 
 object PrivacyPage extends WebPage {
-  override val url: String = Env.host + "/help/privacy"
-  override def isCurrentPage: Boolean = heading == "Privacy policy"
+  override val url: String = "https://www.gov.uk/government/publications/data-protection-act-dpa-information-hm-revenue-and-customs-hold-about-you"
+  override def isCurrentPage: Boolean = heading == "HMRC Privacy Notice"
 
-  def personalInfoText: String = webDriver.findElement(By.id("privacy-personal-info")).getText
+  // Temporary
+  // override def isCurrentPage: Boolean = heading == "Privacy policy"
+  // def personalInfoText: String = webDriver.findElement(By.id("privacy-personal-info")).getText
 }
