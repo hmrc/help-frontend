7be30a29402f9e09f29eb6753ba894daefec8cb9
Andrew Shanks <andrew.shanks@digital.hmrc.gov.uk>
2018-10-15 13:58:49 +0100

    BTA-924: Updating resolvers in attempt to fix failure to acquire library

--- project/MicroService.scala
+++ project/MicroService.scala
@@ -44,7 +44,8 @@ trait MicroService {
       resolvers := Seq(
         Resolver.bintrayRepo("hmrc", "releases"),
         Resolver.typesafeRepo("releases"),
-        Resolver.jcenterRepo
+        Resolver.jcenterRepo,
+        "hmrc-releases" at "https://artefacts.tax.service.gov.uk/artifactory/hmrc-releases/"
       )
     )
     .disablePlugins(sbt.plugins.JUnitXmlReportPlugin)
