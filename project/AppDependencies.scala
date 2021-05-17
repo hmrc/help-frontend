dece46a3f55d8954b2ec81ea9cfd9f93d3165868
Oscar Duignan <100650+oscarduignan@users.noreply.github.com>
2021-05-17 15:34:39 +0100

    PLATUI-1179: update bootstrap version to 5.3.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,17 +1,15 @@
 import play.core.PlayVersion.current
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "4.0.0",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "5.3.0",
     "uk.gov.hmrc" %% "play-frontend-hmrc"         % "0.56.0-play-27"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-27"  % "4.0.0"   % Test,
+    "uk.gov.hmrc"            %% "bootstrap-test-play-27"  % "5.3.0"   % Test,
     "org.scalatest"          %% "scalatest"               % "3.2.3"   % Test,
     "org.scalatestplus"      %% "selenium-3-141"          % "3.2.0.0" % Test,
     "org.jsoup"               % "jsoup"                   % "1.13.1"  % Test,
