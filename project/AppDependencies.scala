b2389a30a9e0e0511ee0672254094596170f783c
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2021-09-14 11:30:13 +0100

    PLATUI-1374: Uplifted version of play-frontend-hmrc

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.6.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "1.0.0-play-28"
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "1.10.0-play-28"
   )
 
   val test = Seq(
