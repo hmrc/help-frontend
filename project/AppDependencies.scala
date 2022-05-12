146683457d85fbaae3d3c7d341bc55b93a109346
Oscar Duignan <100650+oscarduignan@users.noreply.github.com>
2022-05-12 15:25:35 +0100

    PLATUI-1722: update play-frontend-hmrc to 3.17.0
    To get fix for play-language url encoding issue

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.6.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "3.9.0-play-28"
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "3.17.0-play-28"
   )
 
   val test = Seq(
