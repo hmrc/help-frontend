3e5ade124c116785cfbdcae44652f8b97d98e070
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2022-09-05 14:37:51 +0100

    PLATUI-1944: Prompt as notification banner

--- public/app-install-prompt.js
+++ public/app-install-prompt.js
@@ -1,11 +1,14 @@
 let deferredPrompt;
 
 window.addEventListener('load', (e) => {
+    console.log('page load called')
+
     window.addEventListener('beforeinstallprompt', (e) => {
         // Prevent Chrome 67 and earlier from automatically showing the prompt
         e.preventDefault();
         // Stash the event so it can be triggered later.
         deferredPrompt = e;
+        console.log("beforeinstallprompt fired")
         // Update UI notify the user they can add to home screen
         showInstallAppButton()
     });
@@ -26,11 +29,11 @@ function installApp() {
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice
         .then((choiceResult) => {
-            // if (choiceResult.outcome === 'accepted') {
-            //     console.log('User accepted the Install prompt');
-            // } else {
-            //     console.log('User dismissed the Install prompt');
-            // }
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the Install prompt');
+            } else {
+                console.log('User dismissed the Install prompt');
+            }
             deferredPrompt = null;
         });
 }
