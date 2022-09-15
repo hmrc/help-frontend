dd1e6d3836b97d2b3617f174bbfef9aa2483b556
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2022-09-15 13:24:07 +0100

    PLATUI-1975: Updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,18 +3,18 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.6.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "3.17.0-play-28"
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.3.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "3.26.0-play-28"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % "5.6.0"   % Test,
-    "org.scalatest"          %% "scalatest"               % "3.2.3"   % Test,
-    "org.scalatestplus"      %% "selenium-3-141"          % "3.2.0.0" % Test,
-    "org.jsoup"               % "jsoup"                   % "1.13.1"  % Test,
-    "org.mockito"            %% "mockito-scala-scalatest" % "1.14.8"  % Test,
-    "uk.gov.hmrc"            %% "webdriver-factory"       % "0.22.0"  % Test,
-    "com.vladsch.flexmark"    % "flexmark-all"            % "0.36.8"  % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"   % Test
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % "7.3.0"    % Test,
+    "org.scalatest"          %% "scalatest"               % "3.2.13"   % Test,
+    "org.scalatestplus"      %% "selenium-4-2"            % "3.2.13.0" % "test",
+    "org.jsoup"               % "jsoup"                   % "1.13.1"   % Test,
+    "org.mockito"            %% "mockito-scala-scalatest" % "1.14.8"   % Test,
+    "uk.gov.hmrc"            %% "webdriver-factory"       % "0.38.0"   % Test,
+    "com.vladsch.flexmark"    % "flexmark-all"            % "0.62.2"   % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"    % Test
   )
 }
