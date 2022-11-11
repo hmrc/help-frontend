b922a27cc082856c402a98efb2df1cf913fbd8e4
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2022-11-11 11:00:58 +0000

    NOJIRA: Bumped Play version to 2.8.18

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.3.0",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.11.0",
     "uk.gov.hmrc" %% "play-frontend-hmrc"         % "3.26.0-play-28"
   )
 
