24da978d682b06456f7b5abd11bf5a1ed92270c1
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2023-04-28 14:18:53 +0100

    NOJIRA: Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "7.3.0-play-28"
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "7.5.0-play-28"
   )
 
   val test = Seq(
@@ -15,7 +15,7 @@ object AppDependencies {
     "org.scalatestplus"      %% "selenium-4-2"            % "3.2.13.0"       % Test,
     "org.jsoup"               % "jsoup"                   % "1.13.1"         % Test,
     "org.mockito"            %% "mockito-scala-scalatest" % "1.14.8"         % Test,
-    "uk.gov.hmrc"            %% "webdriver-factory"       % "0.39.0"         % Test,
+    "uk.gov.hmrc"            %% "webdriver-factory"       % "0.41.0"         % Test,
     "com.vladsch.flexmark"    % "flexmark-all"            % "0.62.2"         % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"          % Test
   )
