e0ffb1f34d05c826e28ca80a562238ca6c2c3afa
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2023-10-10 16:36:14 +0100

    NOJIRA: Updated library and plugin dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,11 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "7.15.0"
+  private val bootstrapVersion = "7.22.0"
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "7.9.0-play-28"
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "7.23.0-play-28"
   )
 
   val test = Seq(
