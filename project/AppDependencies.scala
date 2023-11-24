983a004c014bd65fb6fc813aa6a9ad5bb8e032a8
Joanna Pinto Paul <joanna.pinto.paul@digital.hmrc.gov.uk>
2023-11-24 11:08:16 +0000

    Nojira upgrade new library builds (#120)
    NOJIRA: Upgrading service to use new major versions of libraries with Play 2.9

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,21 +2,23 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "7.22.0"
+  private val bootstrapVersion = "8.0.0"
+  private val frontendVersion  = "8.0.0"
+  private val playVersion      = "play-29"
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "7.29.0-play-28"
+    "uk.gov.hmrc" %% s"bootstrap-frontend-$playVersion" % bootstrapVersion,
+    "uk.gov.hmrc" %% s"play-frontend-hmrc-$playVersion" % frontendVersion
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % bootstrapVersion % Test,
-    "org.scalatest"          %% "scalatest"               % "3.2.13"         % Test,
-    "org.scalatestplus"      %% "selenium-4-2"            % "3.2.13.0"       % Test,
-    "org.jsoup"               % "jsoup"                   % "1.13.1"         % Test,
-    "org.mockito"            %% "mockito-scala-scalatest" % "1.14.8"         % Test,
-    "uk.gov.hmrc"            %% "webdriver-factory"       % "0.41.0"         % Test,
-    "com.vladsch.flexmark"    % "flexmark-all"            % "0.62.2"         % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"          % Test
+    "uk.gov.hmrc"            %% s"bootstrap-test-$playVersion" % bootstrapVersion % Test,
+    "org.scalatest"          %% "scalatest"                    % "3.2.13"         % Test,
+    "org.scalatestplus"      %% "selenium-4-2"                 % "3.2.13.0"       % Test,
+    "org.jsoup"               % "jsoup"                        % "1.13.1"         % Test,
+    "org.mockito"            %% "mockito-scala-scalatest"      % "1.14.8"         % Test,
+    "uk.gov.hmrc"            %% "webdriver-factory"            % "0.41.0"         % Test,
+    "com.vladsch.flexmark"    % "flexmark-all"                 % "0.62.2"         % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play"           % "5.1.0"          % Test
   )
 }
