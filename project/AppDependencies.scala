1fd751a8ba5a51586799a8443e8621d397a3d5c5
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2023-11-24 10:30:43 +0000

    NOJIRA: Upgrading service to use new major versions of libraries

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,21 +3,22 @@ import sbt._
 object AppDependencies {
 
   private val bootstrapVersion = "8.0.0"
-  private val frontendVersion = "8.0.0"
+  private val frontendVersion  = "8.0.0"
+  private val playVersion      = "play-28"
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-28" % frontendVersion
+    "uk.gov.hmrc" %% s"bootstrap-frontend-$playVersion" % bootstrapVersion,
+    "uk.gov.hmrc" %% s"play-frontend-hmrc-$playVersion" % frontendVersion
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % bootstrapVersion % Test,
-    "org.scalatest"          %% "scalatest"               % "3.2.13"         % Test,
-    "org.scalatestplus"      %% "selenium-4-2"            % "3.2.13.0"       % Test,
-    "org.jsoup"               % "jsoup"                   % "1.13.1"         % Test,
-    "org.mockito"            %% "mockito-scala-scalatest" % "1.14.8"         % Test,
-    "uk.gov.hmrc"            %% "webdriver-factory"       % "0.41.0"         % Test,
-    "com.vladsch.flexmark"    % "flexmark-all"            % "0.62.2"         % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"          % Test
+    "uk.gov.hmrc"            %% s"bootstrap-test-$playVersion" % bootstrapVersion % Test,
+    "org.scalatest"          %% "scalatest"                    % "3.2.13"         % Test,
+    "org.scalatestplus"      %% "selenium-4-2"                 % "3.2.13.0"       % Test,
+    "org.jsoup"               % "jsoup"                        % "1.13.1"         % Test,
+    "org.mockito"            %% "mockito-scala-scalatest"      % "1.14.8"         % Test,
+    "uk.gov.hmrc"            %% "webdriver-factory"            % "0.41.0"         % Test,
+    "com.vladsch.flexmark"    % "flexmark-all"                 % "0.62.2"         % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play"           % "5.1.0"          % Test
   )
 }
