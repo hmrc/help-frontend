4926196563f34ea07daf4b5254759e3011204c5c
Joanna Pinto Paul <joanna.pinto.paul@digital.hmrc.gov.uk>
2023-11-30 15:42:22 +0000

    PLATUI-2664: Upgrade to Play 3.0 (#122)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,9 +2,9 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "8.0.0"
+  private val bootstrapVersion = "8.1.0"
   private val frontendVersion  = "8.0.0"
-  private val playVersion      = "play-29"
+  private val playVersion      = "play-30"
 
   val compile = Seq(
     "uk.gov.hmrc" %% s"bootstrap-frontend-$playVersion" % bootstrapVersion,
@@ -19,6 +19,6 @@ object AppDependencies {
     "org.mockito"            %% "mockito-scala-scalatest"      % "1.14.8"         % Test,
     "uk.gov.hmrc"            %% "webdriver-factory"            % "0.41.0"         % Test,
     "com.vladsch.flexmark"    % "flexmark-all"                 % "0.62.2"         % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play"           % "5.1.0"          % Test
+    "org.scalatestplus.play" %% "scalatestplus-play"           % "7.0.0"          % Test
   )
 }
