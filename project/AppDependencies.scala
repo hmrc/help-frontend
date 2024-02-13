ba19c3fd93c75cb1bc9ce4dff523bf66b9b4a2d4
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2024-02-13 16:24:51 +0000

    PLATUI-2826: Updated to Selenium 4.12

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,11 +13,11 @@ object AppDependencies {
 
   val test = Seq(
     "uk.gov.hmrc"            %% s"bootstrap-test-$playVersion" % bootstrapVersion % Test,
-    "org.scalatest"          %% "scalatest"                    % "3.2.13"         % Test,
-    "org.scalatestplus"      %% "selenium-4-2"                 % "3.2.13.0"       % Test,
+    "org.scalatest"          %% "scalatest"                    % "3.2.17"         % Test,
+    "org.scalatestplus"      %% "selenium-4-12"                % "3.2.17.0"       % Test,
     "org.jsoup"               % "jsoup"                        % "1.13.1"         % Test,
     "org.mockito"            %% "mockito-scala-scalatest"      % "1.14.8"         % Test,
-    "uk.gov.hmrc"            %% "webdriver-factory"            % "0.41.0"         % Test,
+    "uk.gov.hmrc"            %% "webdriver-factory"            % "0.46.0"         % Test,
     "com.vladsch.flexmark"    % "flexmark-all"                 % "0.62.2"         % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"           % "7.0.0"          % Test
   )
