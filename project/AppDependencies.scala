6c9c24b7d5163d36375b17b9c248d1022393997b
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2024-04-15 16:32:03 +0100

    PLATUI-2942: Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object AppDependencies {
 
   private val bootstrapVersion = "8.5.0"
-  private val frontendVersion  = "9.4.0"
+  private val frontendVersion  = "9.6.0"
   private val playVersion      = "play-30"
 
   val compile = Seq(
@@ -17,7 +17,7 @@ object AppDependencies {
     "org.scalatestplus"      %% "selenium-4-12"                % "3.2.17.0"       % Test,
     "org.jsoup"               % "jsoup"                        % "1.13.1"         % Test,
     "org.mockito"            %% "mockito-scala-scalatest"      % "1.14.8"         % Test,
-    "uk.gov.hmrc"            %% "ui-test-runner"               % "0.24.0"         % Test,
+    "uk.gov.hmrc"            %% "ui-test-runner"               % "0.26.0"         % Test,
     "com.vladsch.flexmark"    % "flexmark-all"                 % "0.62.2"         % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"           % "7.0.0"          % Test
   )
