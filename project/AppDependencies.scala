4e9e159335fb9138136a08745809f5acec7bef6d
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2024-08-27 15:28:47 +0100

    PLATUI-3199: Bump version of play-frontend-hmrc and others to latest

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.0.0"
-  private val frontendVersion  = "10.8.0"
+  private val bootstrapVersion = "9.3.0"
+  private val frontendVersion  = "10.10.0"
   private val playVersion      = "play-30"
 
   val compile = Seq(
@@ -17,7 +17,7 @@ object AppDependencies {
     "org.seleniumhq.selenium" % "selenium-java"                % "4.18.1"         % Test,
     "org.jsoup"               % "jsoup"                        % "1.13.1"         % Test,
     "org.scalatestplus"      %% "mockito-3-4"                  % "3.2.10.0"       % Test,
-    "uk.gov.hmrc"            %% "ui-test-runner"               % "0.31.0"         % Test,
+    "uk.gov.hmrc"            %% "ui-test-runner"               % "0.35.0"         % Test,
     "com.vladsch.flexmark"    % "flexmark-all"                 % "0.62.2"         % Test
   )
 }
