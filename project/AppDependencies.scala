44592ddcf5a91d0ac0b8fb31a2592f9b15ccb12e
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2024-11-11 13:51:20 +0000

    PLATUI-3341: Updated play-frontend-hmrc

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "9.5.0"
-  private val frontendVersion  = "11.2.0"
+  private val frontendVersion  = "11.4.0"
   private val playVersion      = "play-30"
 
   val compile = Seq(
