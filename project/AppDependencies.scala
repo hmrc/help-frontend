0f1881f99783e43d2111920d46d2f2203c651989
TimothyFothergill <67912934+TimothyFothergill@users.noreply.github.com>
2025-10-07 13:47:31 +0100

    PLATUI-3978 bump play-frontend-hmrc version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "9.19.0"
-  private val frontendVersion  = "12.15.0"
+  private val frontendVersion  = "12.17.0"
   private val playVersion      = "play-30"
 
   val compile = Seq(
