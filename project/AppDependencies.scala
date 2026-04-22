95b8c487604865532d9dcd95901d75013d11a5a3
Oscar Duignan <100650+oscarduignan@users.noreply.github.com>
2026-04-22 16:06:47 +0100

    bump deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.6.0"
+  private val bootstrapVersion = "10.7.0"
   private val frontendVersion  = "12.32.1"
   private val playVersion      = "play-30"
 
@@ -14,7 +14,7 @@ object AppDependencies {
   val test = Seq(
     "uk.gov.hmrc"         %% s"bootstrap-test-$playVersion" % bootstrapVersion % Test,
     "org.scalatestplus"   %% "mockito-3-4"                  % "3.2.10.0"       % Test,
-    "org.jsoup"            % "jsoup"                        % "1.13.1"         % Test,
-    "com.vladsch.flexmark" % "flexmark-all"                 % "0.62.2"         % Test
+    "org.jsoup"            % "jsoup"                        % "1.22.2"         % Test,
+    "com.vladsch.flexmark" % "flexmark-all"                 % "0.64.8"         % Test
   )
 }
